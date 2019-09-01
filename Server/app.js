1f2f2f2fde20676f2dff567e02845b5eeedcf4b7
Natalia Arias <nataliaarias@Natalias-MacBook-Pro.local>
2019-09-01 00:32:39 -0500

    Pagina cursos lista

---
+++ Server/app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+              app = express(),
+              http = require("http").Server(app).listen(80);
+console.log("Started at port 80")
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/public/html/courses.html");
+})
+
+app.get("/course-details",function(req,res){
+  res.sendFile(__dirname+"/public/html/course-details.html");
+})
